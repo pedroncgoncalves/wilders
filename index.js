44ccad59811455daa8555e7023a9daf14bafcb94
pedroncgoncalves <peterconde21@gmail.comgit config --global user.email peterconde21@gmail.comgit config --global user.email peterconde21@gmail.com>
2021-03-24 15:53:30 +0000

    unbelievable  wilders coloured names

---
+++ index.js
@@ -0,0 +1,6 @@
+const chalk = require('chalk');
+ 
+console.log(chalk.blue('Hello Hugo!'));
+console.log(chalk.red('Hello Filipe!'));
+console.log(chalk.green('Hello Rodrigo!'));
+console.log(chalk.yellow('Hello Nikolina!'));
